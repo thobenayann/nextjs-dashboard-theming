b8c0cb51bc034b64a55862b3d54cae9f60ce797f
thobenayann <thobena.yann@orange.fr>
2025-03-19 23:17:17 +0100

    fix: lint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ActiveThemeProvider } from "@/components/active-theme";
 import { cookies } from "next/headers";
 import { cn } from "@/lib/utils";
 
-const META_THEME_COLORS = {
-  light: "#ffffff",
-  dark: "#09090b",
-};
+// const META_THEME_COLORS = {
+//   light: "#ffffff",
+//   dark: "#09090b",
+// };
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
